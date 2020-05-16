833d7a99c40ae9f0083ebd86d76b003b3f89f150
jihye <jyebe9034@gmail.com>
2020-05-16 21:22:36 +0900

    add weakSet

--- string.js
+++
@@ -1,6 +0,0 @@
-let str = "hellow world ! ^^ ~~";
-let matchstr = "^ ~~";
-// console.log(str.startsWith(matchstr));
-// console.log(str.endsWith(matchstr));
-console.log("include test ", str.includes("^^^"));
-
