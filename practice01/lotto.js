69b4ef4235e8773971a26beb013a5cb3d07d84f6
jihye <jyebe9034@gmail.com>
2020-05-21 21:54:54 +0900

    add lotto template

---
+++ practice01/lotto.js
@@ -0,0 +1,20 @@
+// lotto 번호 만들기
+// 1. 유일한 값을 추출하는 과정에서 Set을 사용함
+// 2. getRandomNumber 함수에 변수를 전달하는 과정에서 destructuring을 사용함
+
+const Setting = {
+  name : 'LUCKY LOTTO',
+  cout : 6,
+  maxNumber : 45
+}
+
+function getRandomNumber(maxNumber) {
+  // 랜덤한 유일한 숫자값을 추출
+  let colorSet = new WeakSet();
+}
+
+for(let i = 0; i < count; i++) {
+  getRandomNumber(maxNumber);
+}
+
+console.log(colorSet.values());
